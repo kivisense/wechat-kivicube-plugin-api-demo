72f6109c6518f5b027fe2d9a245e06de53ec22ad
yubingcen <644268318@qq.com>
2023-03-10 16:58:13 +0800

    feat: 移除图像追踪的fs使用

--- miniprogram/pages/image-tracking/image2d-tracking/image2d-tracking.js
+++ miniprogram/pages/image-tracking/image2d-tracking/image2d-tracking.js
@@ -1,9 +1,5 @@
 // 去除图像追踪功能水印的license
 // 必须在打开场景之前设置好license，否则就会出现水印。
-// 当前图像识别功能需要使用小程序文件管理器, 请参照文档使用 setFileSystemManager 设置。
-
-const { setFileSystemManager } = requirePlugin("kivicube");
-setFileSystemManager(wx.getFileSystemManager(), wx.env.USER_DATA_PATH);
 
 Page({
   data: {
